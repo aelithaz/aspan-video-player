45b42faafb3c75201db6b740ee97374ba35c5e92
aelithaz <anzhang@umass.edu>
2025-04-28 16:07:38 -0400

    Added route for get/

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const { recordView } = require('../controllers/videoController');
 
+router.get('/', (req, res) => {
+    res.send('Welcome to ASPAN Video Player Backend!');
+});
+
 router.post('/api/view', recordView);
 
 module.exports = router;
